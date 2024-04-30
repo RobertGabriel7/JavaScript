b972c1323a9072916140c55fb48d343f60d8c016
Robert <97358106+RobertGabriel7@users.noreply.github.com>
2024-04-29 22:05:40 -0300

    updates...

--- Aulas/Udemy/exercicios/aula_48/assets/js/main.js
+++ Aulas/Udemy/exercicios/aula_48/assets/js/main.js
@@ -1,95 +1,45 @@
-function meuEscopo() {
+function diaDeHoje() {
 
-    function diaDeHoje() {
-        let information1 = window.document.querySelector("#information1")
-        let data = new Date();
-        let dia = data.getUTCDay();
-        let mes = data.getUTCMonth();
-        let ano = data.getFullYear();
-        let hora = data.getHours();
-        let minutos = data.getMinutes();
-        let nuDoDiaDaSemana = data.getDate();
-        let dia_da_semana;
+    let information1 = window.document.querySelector("#information1")
+    let data = new Date();
+    let dia = data.getDay();
+    let mes = data.getUTCMonth();
+    let ano = data.getFullYear();
+    let hora = data.getHours();
+    let minutos = data.getMinutes();
+    let nuDoDiaDaSemana = data.getDate();
 
-
+    function diaDaSemana() {
         switch (dia) {
-            case 1:
-                dia_da_semana = 'Domingo'
-                break;
-
-            case 2:
-                dia_da_semana = 'Segunda-Feira'
-                break;
-
-            case 3:
-                dia_da_semana = 'Terça-Feira';
-                break;
-
-            case 4:
-                dia_da_semana = 'Quarta-Feira';
-                break;
-
-            case 5:
-                dia_da_semana = 'Quinta-Feira';
-                break;
-
-            case 6:
-                dia_da_semana = 'Sexta-Feira'
-                break;
-
-            case 7:
-                dia_da_semana = 'Sábado'
-                break;
-
-            default:
-                window.confirm('É isso ai.')
+            case 0: return dia_da_semana = 'Domingo'
+            case 1: return dia_da_semana = 'Segunda-Feira'
+            case 2: return dia_da_semana = 'Terça-Feira';
+            case 3: return dia_da_semana = 'Quarta-Feira';
+            case 4: return dia_da_semana = 'Quinta-Feira';
+            case 5: return dia_da_semana = 'Sexta-Feira'
+            case 6: return dia_da_semana = 'Sábado'
+            default: window.confirm('É isso ai.')
         }
-
-        switch (mes) {
-            case 0:
-                mes = 'Janeiro';
-                break;
-            case 1:
-                mes = 'Fevereiro';
-                break;
-            case 2:
-                mes = 'Março';
-                break;
-            case 3:
-                mes = 'Abril';
-                break;
-            case 4:
-                mes = 'Maio';
-                break;
-            case 5:
-                mes = 'Junho';
-                break;
-            case 6:
-                mes = 'Julho';
-                break;
-            case 7:
-                mes = 'Agosto';
-                break;
-            case 8:
-                mes = 'Setembro';
-                break;
-            case 9:
-                mes = 'Outubro';
-                break;
-            case 10:
-                mes = 'Novembro';
-                break;
-            case 11:
-                mes = 'Dezembro';
-                break;
-            default:
-                mes = 'Mês inválido';
-        }
-
-        information1.innerHTML = `${dia_da_semana}, ${hora}:${minutos},  ${nuDoDiaDaSemana} de ${mes} de ${ano}.`
     }
 
-    diaDeHoje();
+    function mesDoAno() {
+        switch (mes) {
+            case 0: return omes = 'Janeiro';
+            case 1: return omes = 'Fevereiro';
+            case 2: return omes = 'Março';
+            case 3: return omes = 'Abril';
+            case 4: return omes = 'Maio';
+            case 5: return omes = 'Junho';
+            case 6: return omes = 'Julho';
+            case 7: return omes = 'Agosto';
+            case 8: return omes = 'Setembro';
+            case 9: return omes = 'Outubro';
+            case 10: return omes = 'Novembro';
+            case 11: return omes = 'Dezembro';
+            default: omes = 'Mês inválido';
+        }
+    }
+    information1.innerHTML = `${diaDaSemana()}, ${hora}:${minutos},  ${nuDoDiaDaSemana} de ${mesDoAno().toLocaleLowerCase()} de ${ano}.`
 
 }
-meuEscopo();
+diaDeHoje();
