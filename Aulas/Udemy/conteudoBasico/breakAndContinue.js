cd2e03e820d9dfa364cf3b7bea5932c9f347b5b1
Robert <97358106+RobertGabriel7@users.noreply.github.com>
2024-05-16 22:56:45 -0300

    Updates...

---
+++ Aulas/Udemy/conteudoBasico/breakAndContinue.js
@@ -0,0 +1,12 @@
+/* 
+
+Continue: 
+
+Quando tiver "Continue" dentro de um bloco de código, como uma function, a execução desse código volta ao começa quando encontra a palavra "Continue". 
+
+
+Break:
+
+Na hora que é encontrado a palavra "Break"; a execução do código para.
+
+*/
--- Aulas/Udemy/conteudoBasico/while_and_DoWhile.js
+++ Aulas/Udemy/conteudoBasico/while_and_DoWhile.js
@@ -1,10 +1,12 @@
 let numero = 0
 
-
+/* verifica e executa o código */
 while (numero < 2){
     console.log("Oloco")
     numero++
 }
+
+/* Do while executa o código e depois verificar a condição */
 
 numero = 0 
 
@@ -13,3 +15,6 @@ do {
     console.log("Vish")
     numero++
 } while(numero < 2)
+
+
+/* Como é na realidade: a function while é mais usada quando não sabe ao certo quando vai encontrar algum valor */
